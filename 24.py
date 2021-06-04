6033ed364c7242c574dfd4fb2633ebd61e5a6af2
Navneet Kumar Chaurasiya <32726683+nkwarrior84@users.noreply.github.com>
2021-06-04 15:10:06 +0530

    24.py

---
+++ 24.py
@@ -0,0 +1,21 @@
+'''
+Level 1
+
+Question: Define a class, which have a class parameter and have a same instance parameter.
+
+Hints: Define a instance parameter, need add it in init method You can init a object with construct parameter or set the value later
+'''
+class Person:
+    # Define the class parameter "name"
+    name = "Person"
+    
+    def __init__(self, name = None):
+        # self.name is the instance parameter
+        self.name = name
+
+navneet = Person("Navneet")
+print("%s name is %s" % (Person.name, navneet.name))
+
+nikita = Person()
+nikita.name = "Nico"
+print("%s name is %s" % (Person.name, nikita.name))
