709a600b0f6afed3781242a75fa12fecb1a933e4
Navneet Kumar Chaurasiya <32726683+nkwarrior84@users.noreply.github.com>
2021-08-03 09:21:58 +0530

    timus 2025

--- linefighter.java
+++ linefighter.java
@@ -20,10 +20,12 @@ output
 
 import java.util.Scanner;
 
-public class LineFighting_2025 {
+public class lineFighting {
+    
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         int n = x.nextInt();
+        
         for (int i = 0; i < n; i++) {
             System.out.println(solve(x.nextInt(), x.nextInt()));
         }
@@ -32,9 +34,13 @@ public class LineFighting_2025 {
     public static long solve(int n, int k){
         int a[] = new int[k], t = 0;
         long res = 0;
+        
+        int x = n % k == 0 ? n / k : n % k ;
+        int y = n % k  == 0 ? n / k : n / k + 1;
 
-        for (int i = 0; i < (n % k == 0 ? n / k : n % k); i++) {
-            if (i < a.length)  a[i] = n % k  == 0 ? n / k : n / k + 1;
+        for (int i = 0; i < x; i++) {
+            if (i < a.length)  
+                a[i] = y;
             t++;
         }
 
